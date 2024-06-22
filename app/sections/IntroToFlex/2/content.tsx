bbbc1dff75396a430671de59c337d8472a5851c9
Bardemic <134565367+Bardemic@users.noreply.github.com>
2024-06-22 18:44:19 -0500

    Add Several Lines of CSS Functionality

--- app/sections/IntroToFlex/2/content.tsx
+++ app/sections/IntroToFlex/2/content.tsx
@@ -14,34 +14,55 @@ import {
     CardTitle,
   } from "@/components/ui/card"
 import { Textarea } from "@/components/ui/textarea"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { split } from "postcss/lib/list"
 
 
 
-export default function Content1() {
+export default function Content2() {
     const [code, setCode] = useState<React.CSSProperties>({})
-    const [completed, setCompleted] = useState(false)
+    const [line1completed, setLine1Completed] = useState(false)
+    const [line2completed, setLine2Completed] = useState(false)
+    const [line1, setLine1] = useState('')
+    const [line2, setLine2] = useState('')
 
 
-    function codeLineChange(input: string) {
-        setCode(parseStyleString(input))
+    function codeLineChange() {
+        setCode(parseStyleString(line1, line2))
     }
 
-    const parseStyleString = (styleStr: string): React.CSSProperties => {
-        styleStr = styleStr.toLowerCase()
-        return styleStr.split(';').reduce((acc, style) => {
+    useEffect(() => {
+        codeLineChange()
+    }, [line1, line2])
+
+    const parseStyleString = (styleStr1: string, styleStr2: string): React.CSSProperties => {
+        styleStr1 = styleStr1.toLowerCase()
+        styleStr2 = styleStr2.toLowerCase()
+        const css1 = styleStr1.split(';').reduce((acc, style) => {
           if (style.trim()) {
             const [key, value] = style.split(':');
             if (key && value) {
               (acc as any)[key.trim()] = value.trim();
               if(key == 'justify-content' && value == ' center') {
-                setCompleted(true)
+                setLine1Completed(true)
               }
             }
           }
           return acc;
         }, {} as React.CSSProperties);
+        const css2 = styleStr2.split(';').reduce((acc, style) => {
+            if (style.trim()) {
+              const [key, value] = style.split(':');
+              if (key && value) {
+                (acc as any)[key.trim()] = value.trim();
+                if(key == 'align-items' && value == ' center') {
+                  setLine2Completed(true)
+                }
+              }
+            }
+            return acc;
+          }, {} as React.CSSProperties);
+          return {...css1, ...css2}
       };
 
 
@@ -55,15 +76,16 @@ export default function Content1() {
             </Card>
             <Card className="row-start-2 bg-secondary p-2">
                 <p>#container &#123;</p>
-                <div className="mx-2 ml-6">
+                <div className="mx-2 flex flex-col gap-1 ml-6">
                     <p>Padding: 24px;</p>
                     <p>Display: flex;</p>
-                    <input className="w-full bg-primary-foreground text-black focus:outline-none" type="text" onChangeCapture={(e) => codeLineChange(e.currentTarget.value)} />
+                    <input className="w-full bg-primary-foreground text-black focus:outline-none" type="text" onChangeCapture={(e) => setLine1(e.currentTarget.value)} />
+                    <input className="w-full bg-primary-foreground text-black focus:outline-none" type="text" onChangeCapture={(e) => setLine2(e.currentTarget.value)} />
                 </div>
-                {!completed ? <p>&#125;</p> : <></>}
+                <p>&#125;</p>
             </Card>
             <div className="relative row-span-2">
-                <Card className="w-full h-full bg-primary flex justify-center">
+                <Card className="w-full h-full bg-primary flex items-center justify-center">
                     <CardHeader>
                     <Card className="flex h-24 w-24 items-center justify-center bg-transparent border-dotted border-4 text-center">
                         over here
@@ -81,7 +103,7 @@ export default function Content1() {
           </main>
           <footer className="grid grid-cols-2 gap-32 w-full p-2">
           <Button variant="destructive">Previous</Button>
-          {completed ? <Button>Next</Button> : <Button variant='outline' disabled>Next</Button>}
+          {line1completed && line2completed ? <Button>Next</Button> : <Button variant='outline' disabled>Next</Button>}
         </footer>
     </div>
     )
---
+++ app/sections/IntroToFlex/2/page.tsx
@@ -0,0 +1,25 @@
+import AuthButton from "@/components/AuthButton"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import Header from "@/components/Header"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Textarea } from "@/components/ui/textarea"
+import Content2 from "./content"
+
+
+export default function Test1() {
+    return (
+    <div className="flex flex-col items-center h-screen">
+        <Header></Header>
+        <Content2></Content2>
+</div>
+    )
+}
